f2257702a4de624c7facd8f4f211c67f44d098eb
Nicolas Arancibia <105812178+NicolasArancibiaSolis@users.noreply.github.com>
2022-07-08 18:11:14 -0400

    ejercicio

---
+++ javascript.js
@@ -0,0 +1,16 @@
+alert('bienvenido a sagitarius')
+const nombre = prompt('cual es tu nombre?')
+
+const edad = prompt('cual es tu edad?')
+
+const saludo =' bienvenido ' + nombre 
+
+alert(saludo + ' '+ edad + ' años')
+
+const input = prompt ('Ingrese un texto')
+
+console.log (saludo )
+console.log (edad)
+console.log(input.lenght)
+
+alert(' su texto es de ' + input.length + ' caracteres' )
