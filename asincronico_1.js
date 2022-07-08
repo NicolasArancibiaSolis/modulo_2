6c6ad20d0fedb675460ea575c93de51fb74ba354
Nicolas Arancibia <105812178+NicolasArancibiaSolis@users.noreply.github.com>
2022-07-08 18:54:13 -0400

    asincronicos_1

---
+++ asincronico_1.js
@@ -0,0 +1,23 @@
+const edad = prompt('indique su edad')
+
+const edadxdias = (edad * 365)
+
+const mensaje = ('he vivido ' + edadxdias + 'dias')
+
+console.log(edad)
+
+console.log(mensaje)
+
+alert (mensaje)
+
+//segundo ejercicio 
+
+const snack = prompt('mi snack favorito es')
+
+let snackxdia = 2
+
+const cuantos_me_quedan_por_comer_en_mi_vida = ('necesitaras  ' + ' ' +snackxdia*edadxdias + ' ' + snack
++ ' ' + ' para que te alcancen hasta los 82 ')
+alert (cuantos_me_quedan_por_comer_en_mi_vida)
+let precio = 1000 
+const precioxvida = (precio*edadxdias)
